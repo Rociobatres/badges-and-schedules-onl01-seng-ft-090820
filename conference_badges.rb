078f61488da2f16af67e99e56977deed035fc121
Rocio Batres <jonasgirl21@gmail.com>
2020-09-16 18:35:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+#attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def batch_badge_creator(attendees)
+  attendees.map { |name| badge_maker(name) } 
+end 
+
+def assign_rooms(speakers)
+  speakers.map.with_index do |name, i|
+    "Hello, #{name}! You'll be assigned to room #{i + 1}!"
+  end   
+end 
+
+def printer(attendees)
+  
+end   
+
+
